9b83e6c822454bde04881f9b540d4af2f0c0498f
martin väli <martin.vali12@gmail.com>
2024-04-16 11:06:03 +0300

    set main container height to screen height

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <main className="bg-primary">
+    <main className="bg-primary h-screen">
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
