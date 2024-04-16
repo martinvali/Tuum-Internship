77c8b81f161ab617fc77205a512d2ff122810e0d
martin väli <martin.vali12@gmail.com>
2024-04-16 11:09:45 +0300

    add form's media enquiries' and career questions' elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="bg-primary h-screen">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0">
+        <div>
+          <p className="">Media enquiries:</p>
+          <a className="" href="mailto:press@tuum.com">
+            press@tuum.com
+          </a>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <p className="">Career questions:</p>
+        <a className="" href="mailto:careers@tuum.com">
+          careers@tuum.com
+        </a>
+      </div>
     </main>
   );
 }
