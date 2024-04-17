db78b0301c3f7ed7a59c2931e2aacef7935ebebd
martin väli <martin.vali12@gmail.com>
2024-04-17 11:53:23 +0300

    add beginning of form css & html

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,11 @@ function App() {
           </a>
         </div>
       </div>
+      <div className="bg-white tall:py-6 tall-md:py-8 py-8 px-8 lg:px-10 xl:px-16 rounded-[40px]">
+        <fieldset className="mb-8">
+          <h1 className="text-5xl font-recklessNeue">Contact us</h1>
+        </fieldset>
+      </div>
     </main>
   );
 }
