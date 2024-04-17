53c9ddd966ef724627f41b78b3b9626aa680b862
martin väli <martin.vali12@gmail.com>
2024-04-17 11:34:29 +0300

    top contact text area CSS improvements

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,19 @@ import "./App.css";
 function App() {
   return (
     <main className="bg-primary h-screen px-4 text-black-primary">
-      <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0">
+      <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0 pt-[6%]">
         <div>
-          <p className="font-denim">Media enquiries:</p>
-          <a className="" href="mailto:press@tuum.com">
+          <p className="font-denim text-xl">Media enquiries:</p>
+          <a className="text-lg" href="mailto:press@tuum.com">
             press@tuum.com
           </a>
         </div>
-      </div>
-      <div>
-        <p className="">Career questions:</p>
-        <a className="" href="mailto:careers@tuum.com">
-          careers@tuum.com
-        </a>
+        <div>
+          <p className="text-xl">Career questions:</p>
+          <a className="text-lg" href="mailto:careers@tuum.com">
+            careers@tuum.com
+          </a>
+        </div>
       </div>
     </main>
   );
