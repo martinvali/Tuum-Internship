156d24cef1a4fbca2fde2013c986cb0848a1b652
martin väli <martin.vali12@gmail.com>
2024-04-17 13:37:18 +0300

    add close icon. Further improve CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ function App() {
   return (
     <main className="bg-primary h-screen px-4 text-black-primary">
       <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0 pt-[6%]">
-        <div>
+        <button
+          aria-label="close"
+          className="absolute z-50 bg-close-icon bg-center bg-contain	bg-no-repeat right-[20px] sm:right-[70px] md:right-[102px] lg:right-[108px] xl:right-[172px] lg:top-[52px] w-10 h-10"
+        ></button>
+        <div className="!mt-0">
           <p className="font-denim text-xl">Media enquiries:</p>
           <a className="text-lg" href="mailto:press@tuum.com">
             press@tuum.com
           </a>
         </div>
         <div>
-          <p className="text-xl">Career questions:</p>
+          <p className="font-denim text-xl">Career questions:</p>
           <a className="text-lg" href="mailto:careers@tuum.com">
             careers@tuum.com
           </a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,9 @@ export default {
           "Times New Roman",
         ],
       },
+      backgroundImage: {
+        "close-icon": "url('/icon-close.svg')",
+      },
     },
   },
   plugins: [],
