fdcbcb1073bac16f9e511cc3fe4737660c454617
martin väli <martin.vali12@gmail.com>
2024-04-18 12:09:11 +0300

    add mantine components library and basic form input components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import "./App.css";
-
+import "./freakflags.css";
+import { Select, SelectProps, Group, TextInput, Textarea } from "@mantine/core";
 function App() {
+  const data = [{ value: "DZ", label: "Algeria" }];
+
+  const renderSelectOption: SelectProps["renderOption"] = ({ option }) => (
+    <Group flex="1" gap="xs">
+      {option.label}
+      <div className={`fflag fflag-${option.value} w-[21px] h-[13.5px]`}></div>
+    </Group>
+  );
+
   return (
     <main className="bg-primary h-screen px-4 text-black-primary">
       <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0 pt-[6%]">
@@ -25,6 +35,19 @@ function App() {
         <fieldset className="mb-8">
           <h1 className="text-5xl font-recklessNeue">Contact us</h1>
         </fieldset>
+        <TextInput placeholder="First name" />
+        <TextInput placeholder="Last name" />
+        <TextInput placeholder="Email" />
+        <TextInput placeholder="Job title" />
+
+        <Select
+          label="Your favorite library"
+          placeholder="Country"
+          data={data}
+          renderOption={renderSelectOption}
+        />
+        <TextInput placeholder="Company name" />
+        <Textarea placeholder="What would you like to talk about?" />
       </div>
     </main>
   );
