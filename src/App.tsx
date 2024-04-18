ce6e7867d376bd6599157ab3ffa0ec360e4ada21
martin väli <martin.vali12@gmail.com>
2024-04-18 16:24:39 +0300

    work on responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -19,115 +19,121 @@ function App() {
   );
 
   return (
-    <main className="bg-primary h-screen px-4 text-black-primary">
-      <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0 pt-[6%]">
+    <main className="bg-primary h-screen overflow-hidden">
+      <div className="px-4 sm:px-14 md:px-24 lg:px-[72px] xl:px-28 text-black-primary lg:flex lg:flex-row mt-[6%] lg:justify-center lg:items-start relative max-w-7xl mx-auto">
         <button
           aria-label="close"
           className="absolute z-50 bg-close-icon bg-center bg-contain	bg-no-repeat right-[20px] sm:right-[70px] md:right-[102px] lg:right-[108px] xl:right-[172px] lg:top-[52px] w-10 h-10"
         ></button>
-        <div className="!mt-0">
-          <p className="font-denim text-xl">Media enquiries:</p>
-          <a className="text-lg" href="mailto:press@tuum.com">
-            press@tuum.com
-          </a>
-        </div>
-        <div>
-          <p className="font-denim text-xl">Career questions:</p>
-          <a className="text-lg" href="mailto:careers@tuum.com">
-            careers@tuum.com
-          </a>
-        </div>
-      </div>
-      <div className="bg-white tall:py-6 tall-md:py-8 py-8 px-8 lg:px-10 xl:px-16 rounded-[40px]">
-        <fieldset className="mb-8">
-          <h1 className="text-5xl font-recklessNeue">Contact us</h1>
-        </fieldset>
-        <div className="flex flex-row justify-center items-center gap-x-4 mb-4">
-          <TextInput
-            classNames={{
-              input: "input-styles",
-            }}
-            className="flex-1"
-            placeholder="First name"
-          />
-          <TextInput
-            classNames={{
-              input: "input-styles",
-            }}
-            className="flex-1"
-            placeholder="Last name"
-          />
-        </div>
-        <div className="flex flex-row justify-center items-center gap-x-4 mb-4">
-          <TextInput
-            classNames={{
-              input: "input-styles",
-            }}
-            className="flex-1"
-            placeholder="Email"
-          />
-          <TextInput
-            classNames={{
-              input: "input-styles",
-            }}
-            className="flex-1"
-            placeholder="Job title"
-          />
-        </div>
-        <div className="flex flex-row justify-center items-center gap-x-4 mb-4">
-          <Select
-            classNames={{
-              input: "input-styles",
-            }}
-            className="flex-1"
-            placeholder="Country"
-            data={data}
-            renderOption={renderSelectOption}
-          />
-          <TextInput
-            classNames={{
-              input: "input-styles",
-            }}
-            className="flex-1"
-            placeholder="Company name"
-          />
-        </div>
-        <Textarea
-          classNames={{
-            input: "input-styles h-36",
-          }}
-          placeholder="What would you like to talk about?"
-        />
-        <Checkbox
-          size="xs"
-          className="mt-4"
-          label={
-            <p className="font-denim text-base">
-              By submitting this form I accept{" "}
-              <a className="text-[#219dfb] underline" href="">
-                privacy policy
-              </a>{" "}
-              and{" "}
-              <a className="text-[#219dfb] underline" href="">
-                cookie policy.
-              </a>
-              {" *"}
+        <div className="space-y-4 pb-7 text-center sm:text-left md:space-y-7 lg:pb-0 lg:flex-1 lg:basis-[35%]">
+          <div className="!mt-0">
+            <p className="font-denim text-xl md:font-recklessNeue md:text-4xl">
+              Media enquiries:
             </p>
-          }
-        />
+            <a className="text-lg" href="mailto:press@tuum.com">
+              press@tuum.com
+            </a>
+          </div>
+          <div>
+            <p className="font-denim text-xl md:font-recklessNeue md:text-4xl">
+              Career questions:
+            </p>
+            <a className="text-lg" href="mailto:careers@tuum.com">
+              careers@tuum.com
+            </a>
+          </div>
+        </div>
+        <form className="bg-white tall:py-6 tall-md:py-8 py-8 px-8 lg:px-10 xl:px-16 rounded-[40px] lg:flex-1 lg:basis-[65%]">
+          <fieldset className="mb-8">
+            <h1 className="text-5xl font-recklessNeue">Contact us</h1>
+          </fieldset>
+          <div className="flex flex-row justify-center items-center gap-x-4 mb-4">
+            <TextInput
+              classNames={{
+                input: "input-styles",
+              }}
+              className="flex-1"
+              placeholder="First name"
+            />
+            <TextInput
+              classNames={{
+                input: "input-styles",
+              }}
+              className="flex-1"
+              placeholder="Last name"
+            />
+          </div>
+          <div className="flex flex-row justify-center items-center gap-x-4 mb-4">
+            <TextInput
+              classNames={{
+                input: "input-styles",
+              }}
+              className="flex-1"
+              placeholder="Email"
+            />
+            <TextInput
+              classNames={{
+                input: "input-styles",
+              }}
+              className="flex-1"
+              placeholder="Job title"
+            />
+          </div>
+          <div className="flex flex-row justify-center items-center gap-x-4 mb-4">
+            <Select
+              classNames={{
+                input: "input-styles",
+              }}
+              className="flex-1"
+              placeholder="Country"
+              data={data}
+              renderOption={renderSelectOption}
+            />
+            <TextInput
+              classNames={{
+                input: "input-styles",
+              }}
+              className="flex-1"
+              placeholder="Company name"
+            />
+          </div>
+          <Textarea
+            classNames={{
+              input: "input-styles h-36",
+            }}
+            placeholder="What would you like to talk about?"
+          />
+          <Checkbox
+            size="xs"
+            className="mt-4"
+            label={
+              <p className="font-denim text-base">
+                By submitting this form I accept{" "}
+                <a className="text-[#219dfb] underline" href="">
+                  privacy policy
+                </a>{" "}
+                and{" "}
+                <a className="text-[#219dfb] underline" href="">
+                  cookie policy.
+                </a>
+                {" *"}
+              </p>
+            }
+          />
 
-        <Checkbox
-          size="xs"
-          className="mt-4"
-          label={
-            <p className="font-denim text-base">
-              I would like to receive your newsletter.
-            </p>
-          }
-        />
-        <button className="mt-4 bg-primary text-base font-denim py-2.5 px-[1.125rem] rounded-xl hover:bg-[#d0d5dd] color-[#140923]">
-          Submit
-        </button>
+          <Checkbox
+            size="xs"
+            className="mt-4"
+            label={
+              <p className="font-denim text-base">
+                I would like to receive your newsletter.
+              </p>
+            }
+          />
+          <button className="mt-4 bg-primary text-base font-denim py-2.5 px-[1.125rem] rounded-xl hover:bg-[#d0d5dd] color-[#140923]">
+            Submit
+          </button>
+        </form>
       </div>
     </main>
   );
