4396d97cff0468c86820afc291a8d8ed1c1284ff
martin väli <martin.vali12@gmail.com>
2024-04-23 10:00:21 +0300

    improve padding bottom

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ import Form from "./components/form/Form";
 
 function App() {
   return (
-    <main className="bg-primary h-screen overflow-hidden">
+    <main className="bg-primary h-screen overflow-x-hidden">
       <div className="px-4 md:px-8 relative max-w-7xl mx-auto">
         <button
           aria-label="close"
           className="absolute z-50 bg-close-icon bg-center bg-contain	bg-no-repeat right-[20px] sm:right-[70px] md:right-[102px] lg:right-[108px] xl:right-[172px] lg:top-[52px] w-10 h-10 focus-visible:outline-black-primary/50"
         ></button>
-        <div className="mt-[6%] mediumHeight:mt-[3%] tallHeight:mt-[6%] sm:px-10 md:px-16 lg:px-10 xl:px-20 lg:flex lg:flex-row lg:justify-center lg:items-start">
+        <div className="mt-[6%] mediumHeight:mt-[3%] tallHeight:mt-[6%] pb-[6%] mediumHeight:pb-[3%] sm:px-10 md:px-16 lg:px-10 xl:px-20 lg:flex lg:flex-row lg:justify-center lg:items-start">
           <ContactTexts />
           <Form />
         </div>
